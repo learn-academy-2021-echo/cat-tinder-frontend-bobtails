f8360c074361e2c987ebdd06ffed57373ef43a7e
kickeyhickey <irishmanhickey@gmail.com>
2022-02-03 18:46:44 -0800

    create functionality, we can add a new cat

--- src/pages/BirdNew.js
+++ src/pages/BirdNew.js
@@ -1,10 +1,81 @@
 import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from 'reactstrap'
 
 class BirdNew extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        newBird: {
+          name:"",
+          age:"",
+          enjoys:"",
+          image:""
+        }
+
+
+      }
+    }
+
+    handleChange = (e) =>{
+      let { newBird } = this.state
+      newBird[ e.target.name ] = e.target.value
+      this.setState({ newBird: newBird })
+    }
+
+    handleSubmit = () => {
+      this.props.createBird(this.state.newBird)
+    }
+
   render() {
+    console.log(this.state.newBird.name)
     return (
       <>
         <h1>BirdNew</h1>
+        <Form>
+           <FormGroup>
+            <Label for="name">Name</Label>
+            <Input
+             type="text"
+              name="name"
+              onChange={this.handleChange}
+              value = {this.state.newBird.name}
+             />
+           </FormGroup>
+           <FormGroup>
+            <Label for="age">Age</Label>
+            <Input
+             type="text"
+              name="age"
+              onChange={this.handleChange}
+              value = {this.state.newBird.age}
+             />
+           </FormGroup>
+           <FormGroup>
+            <Label for="enjoys">Enjoys</Label>
+            <Input
+             type="text"
+              name="enjoys"
+              onChange={this.handleChange}
+              value = {this.state.newBird.enjoys}
+             />
+           </FormGroup>
+           <FormGroup>
+            <Label for="image">Pic</Label>
+            <Input
+             type="text"
+              name="image"
+              onChange={this.handleChange}
+              value = {this.state.newBird.image}
+             />
+           </FormGroup>
+           <button onClick={this.handleSubmit} name='submit button'>Add a New Bird</button>
+        </Form>
+        {this.state.submitted && <redirect to='/birdindex' />}
       </>
     );
   }
